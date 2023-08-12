27d0ac9b09896ad66690046645f108684a69f15b
jagriti-sethia <jain99jagriti@gmail.com>
2023-08-12 22:04:01 +0530

    update addproduct

--- src/pages/addproduct.jsx
+++ src/pages/addproduct.jsx
@@ -2,7 +2,115 @@ import React from 'react'
 
 const Addproduct = () => {
   return (
-    <div>addproduct</div>
+    <div>
+        <div className='addproduct'>
+        <h3>Add product</h3>
+
+ <form >
+ <label htmlFor="department">Department:</label>
+        <select
+          id="department"
+          name="department"
+          value={newProduct.department}
+          onChange={handleInput}
+          required
+        >
+          <option value="">Select Department</option>
+          <option value="Clothing">Clothing</option>
+          <option value="Toys">Toys</option>
+          <option value="Kitchen">Kitchen</option>
+        </select>
+
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={newProduct.name}
+          onChange={handleInput}
+          required
+        />
+
+        <label htmlFor="description">Description:</label>
+        <textarea
+          id="description"
+          name="description"
+          value={newProduct.description}
+          onChange={handleInput}
+          required
+        />
+
+        <label htmlFor="price">Price:</label>
+        <input
+          type="number"
+          step="0.01"
+          id="price"
+          name="price"
+          value={newProduct.price}
+          onChange={handleInput}
+          required
+        />
+
+        <label htmlFor="stock">Stock:</label>
+        <input
+          type="number"
+          id="stock"
+          name="stock"
+          value={newProduct.stock}
+          onChange={handleInput}
+          required
+        />
+
+        <label htmlFor="sku">SKU:</label>
+        <input
+          type="text"
+          id="sku"
+          name="sku"
+          value={newProduct.sku}
+          onChange={handleInput}
+          required
+        />
+
+        <label htmlFor="supplier">Supplier:</label>
+        <input
+          type="text"
+          id="supplier"
+          name="supplier"
+          value={newProduct.supplier}
+          onChange={handleInput}
+          required
+        />
+
+        <label htmlFor="delivered">Delivered:</label>
+        <input
+          type="number"
+          id="delivered"
+          name="delivered"
+          value={newProduct.delivered}
+          onChange={handleInput}
+          required
+        />
+
+        <label htmlFor="imageUrl">Image URL:</label>
+        <input
+          type="url"
+          id="imageUrl"
+          name="imageUrl"
+          value={newProduct.imageUrl}
+          onChange={handleInput}
+          required
+        />
+
+        <button type="submit">Add Product</button>
+
+
+ </form>
+
+        </div>
+
+
+
+    </div>
   )
 }
 
